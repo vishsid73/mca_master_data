b3611360f13c97e9da0f9c4b0032e439589d2e4f
Siddharth Srivastava <vish.sid73@gmail.com>
2015-05-24 05:57:46 +0530

    prettified json response using filter

--- js/controllers.js
+++ js/controllers.js
@@ -7,9 +7,10 @@ angular.module('myApp.controllers', []).
           var url = "http://ec2-54-169-91-224.ap-southeast-1.compute.amazonaws.com/mca.php?cin="+$scope.cin;
           $http({
     method: 'GET',
-    url: url,
+    url: url
 })
-    .success(function(response) {$scope.response = response;})
+    .success(function(response) {$scope.response = true;
+                  $scope.json = JSON.stringify(response, undefined, 4);})
     .error(function(response) {console.log(response);});  
       };
           
--- js/filters.js
+++ js/filters.js
@@ -2,9 +2,32 @@
 
 /* Filters */
 
-angular.module('myApp.filters', []).
+angular.module('myApp.filters', ['ngSanitize']).
   filter('interpolate', ['version', function(version) {
     return function(text) {
       return String(text).replace(/\%VERSION\%/mg, version);
     }
-  }]);
+  }])
+    .filter('prettify', function () {
+
+      function syntaxHighlight(json) {
+        json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+        return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+          var cls = 'number';
+          if (/^"/.test(match)) {
+            if (/:$/.test(match)) {
+              cls = 'key';
+            } else {
+              cls = 'string';
+            }
+          } else if (/true|false/.test(match)) {
+            cls = 'boolean';
+          } else if (/null/.test(match)) {
+            cls = 'null';
+          }
+          return '<span class="' + cls + '">' + match + '</span>';
+        });
+      }
+
+      return syntaxHighlight;
+    });;
